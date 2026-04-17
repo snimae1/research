0dda54b8dce3091c70dc318cd1408e715b36ce20
snimae1 <snimae1@googlemail.com>
2026-04-17 15:21:42 +0200

    Create DS_V1.py

---
+++ Deepseek/3823/DS_V1.py
@@ -0,0 +1,33 @@
+
+class Solution(object):
+    def reverseByType(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # Separate letters and special characters
+        letters = []
+        specials = []
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                letters.append(ch)
+            else:
+                specials.append(ch)
+        
+        # Reverse both lists
+        letters.reverse()
+        specials.reverse()
+        
+        # Rebuild the string using the reversed lists
+        result = []
+        letter_idx = 0
+        special_idx = 0
+        for ch in s:
+            if 'a' <= ch <= 'z':
+                result.append(letters[letter_idx])
+                letter_idx += 1
+            else:
+                result.append(specials[special_idx])
+                special_idx += 1
+                
+        return ''.join(result)
