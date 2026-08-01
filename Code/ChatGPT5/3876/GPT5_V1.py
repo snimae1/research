4822f488f74f3fbce81c95a0b572dbb1151ba68f
snimae1 <snimae1@googlemail.com>
2026-08-01 09:36:29 +0200

    Update GPT5_V1.py

--- Code/ChatGPT5/3876/GPT5_V1.py
+++ Code/ChatGPT5/3876/GPT5_V1.py
@@ -1,1 +1,27 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        min_odd = None
 
+        # Kleinste ungerade Zahl bestimmen
+        for x in nums1:
+            if x % 2 == 1:
+                if min_odd is None or x < min_odd:
+                    min_odd = x
+
+        # Prüft, ob alle Zahlen auf die Zielparität gebracht werden können.
+        # target = 0 -> alle gerade
+        # target = 1 -> alle ungerade
+        def can_make(target):
+            for x in nums1:
+                if x % 2 != target:
+                    # Parität muss geändert werden.
+                    # Das ist nur möglich, wenn es eine kleinere ungerade Zahl gibt.
+                    if min_odd is None or min_odd >= x:
+                        return False
+            return True
+
+        return can_make(0) or can_make(1)
