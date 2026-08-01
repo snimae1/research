ce863aee7a568ae773abae9966841475b9e78442
snimae1 <snimae1@googlemail.com>
2026-08-01 10:32:41 +0200

    Update GPT5_V1.py

--- Code/ChatGPT5/3884/GPT5_V1.py
+++ Code/ChatGPT5/3884/GPT5_V1.py
@@ -1,1 +1,13 @@
+class Solution(object):
+    def firstMatchingIndex(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
 
+        for i in range(n):
+            if s[i] == s[n - i - 1]:
+                return i
+
+        return -1
