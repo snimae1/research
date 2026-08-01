84d42b8168e078cea16e0a22769dfc9db4d3f0a0
snimae1 <snimae1@googlemail.com>
2026-08-01 10:33:28 +0200

    Create DS_V1.py

---
+++ Deepseek/3884/DS_V1.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def firstMatchingIndex(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        for i in range(n):
+            if s[i] == s[n - i - 1]:
+                return i
+        return -1
