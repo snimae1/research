5b6d81bca1cb5948d4a394d8e637541817d69797
snimae1 <snimae1@googlemail.com>
2026-08-01 09:35:47 +0200

    Update DS_V1.py

--- Deepseek/3876/DS_V1.py
+++ Deepseek/3876/DS_V1.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        # Check if all numbers are even
+        all_even = all(x % 2 == 0 for x in nums1)
+        # Find the global minimum
+        min_val = min(nums1)
+        # Possible if all even or the smallest number is odd
+        return all_even or (min_val % 2 == 1)
