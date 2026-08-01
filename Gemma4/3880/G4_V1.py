258f3aa64266a613a994708319519181c3a512a7
snimae1 <snimae1@googlemail.com>
2026-08-01 10:10:12 +0200

    Update G4_V1.py

--- Gemma4/3880/G4_V1.py
+++ Gemma4/3880/G4_V1.py
@@ -1,1 +1,29 @@
+class Solution(object):
 
+    def minAbsoluteDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """ 
+        # Initialisierung des minimalen Abstands mit Unendlich
+        min_diff = float('inf')
+        
+        # Variablen, um den letzten Index von 1 und 2 zu speichern
+        last_one = -1
+        last_two = -1
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                last_one = i
+                # Wenn wir bereits eine 2 gefunden haben, berechne den Abstand
+                if last_two != -1:
+                    min_diff = min(min_diff, abs(i - last_two))
+            
+            elif nums[i] == 2:
+                last_two = i
+                # Wenn wir bereits eine 1 gefunden haben, berechne den Abstand
+                if last_one != -1:
+                    min_diff = min(min_diff, abs(i - last_one))
+
+        # Wenn min_diff immer noch Unendlich ist, wurde kein gültiges Paar gefunden
+        return min_diff if min_diff != float('inf') else -1
