1ba53ef73b7c014229ade362ad44fa6ffedfb44f
snimae1 <snimae1@googlemail.com>
2026-08-02 17:36:07 +0200

    Delete Code/ChatGPT5/3890/Error_V1.py

--- Code/ChatGPT5/3890/Error_V1.py
+++
@@ -1,7 +0,0 @@
-ImportError: cannot import name isqrt
-    from math import isqrt
-Line 7 in findGoodIntegers (Solution.py)
-    ret = Solution().findGoodIntegers(param_1)
-Line 61 in _driver (Solution.py)
-    _driver()
-Line 75 in <module> (Solution.py)
