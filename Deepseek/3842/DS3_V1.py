1a473775adfcc86b1a92d98c956926cbc72767c2
snimae1 <snimae1@googlemail.com>
2026-08-03 12:44:24 +0200

    Update DS3_V1.py

--- Deepseek/3842/DS3_V1.py
+++ Deepseek/3842/DS3_V1.py
@@ -1,1 +1,17 @@
-
+class Solution(object):
+    def toggleLightBulbs(self, bulbs):
+        """
+        :type bulbs: List[int]
+        :rtype: List[int]
+        """
+        # Set für die aktuell eingeschalteten Lampen
+        on = set()
+        
+        for b in bulbs:
+            if b in on:
+                on.remove(b)   # War an → ausschalten
+            else:
+                on.add(b)      # War aus → einschalten
+                
+        # Sortierte Liste der eingeschalteten Lampen zurückgeben
+        return sorted(on)
