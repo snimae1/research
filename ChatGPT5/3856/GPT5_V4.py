27550a00e842050dd23b83cf60e4a6cdc1bbf5f8
snimae1 <snimae1@googlemail.com>
2026-09-10 12:55:43 +0200

    Create GPT5_V4.py

---
+++ ChatGPT5/3856/GPT5_V4.py
@@ -0,0 +1,22 @@
+"""Solution for removing trailing vowels from a string."""
+
+
+class Solution:
+    """Provides a method for removing trailing vowels."""
+
+    def trimTrailingVowels(self, s):
+        """
+        Remove all vowels from the end of the given string.
+
+        :param s: A string containing lowercase English letters.
+        :return: The string without trailing vowels.
+        """
+        vowels = "aeiou"
+        end = len(s)
+
+        # Move backwards while the current character is a vowel.
+        while end > 0 and s[end - 1] in vowels:
+            end -= 1
+
+        # Return the string without its trailing vowels.
+        return s[:end]
